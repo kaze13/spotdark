64c3a8e901f3128c4a16c1019c23d26d913980b2
Rey <chen_m@worksap.co.jp>
2014-12-15 18:13:00 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function() {
+	// var child_process = require('child_process');
+
+	// var child = child_process.execFile('/etc/alternatives/locate', ['.', '-l', '10'], {}, function(err, stdout, stderr) {
+	// 	if (err) {
+	// 		throw err;
+	// 	} else {
+
+	// 	}
+	// })
+
+	// child.stdout.on('data', function(data) {
+	// 	console.log('stdout: ' + data);
+	// 	var result = data.split('\n');
+	// 	console.log('result array:' + result);
+	// 	document.write(result);
+	// });
+
+	
+})()
