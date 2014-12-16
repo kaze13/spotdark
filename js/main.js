cc01f36cf172955f0efa349291868f89fc7ccad8
Rey <chen_m@worksap.co.jp>
2014-12-16 19:25:51 +0800

    select item

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('mainCtrl', function($scope) {
+    $scope.inputKeypress = function(event) {
+        // enter
+        if (event.keyCode === 13) {
+            $scope.searchFile($scope.keyword);
+        }
+        // down arrow
+        if (event.keyCode === 40) {
+            $scope.selectSearchResult(0);
+        }
+    }
 
-	$scope.keypress = function(event) {
-		if (event.keyCode === 13) {
-			console.log('enter pressed;');
-			$scope.searchFile($scope.keyword);
-		}
-	}
+    $scope.searchFile = function(keyword) {
+        console.warn(keyword);
+        var child_process = require('child_process');
+        var child = child_process.exec('locate ' + keyword + ' -l 10', {}, function(err, stdout, stderr) {
+            $scope.$apply(function() {
+                if (err) {
+                    throw err;
+                } else {
+                    var result = stdout.split('\n');
+                    $scope.searchResult = result;
+                    console.log('$scope.searchResult:' + $scope.searchResult);
 
-	$scope.searchFile = function(keyword) {
-		console.warn(keyword);
-		var child_process = require('child_process');
-		var child = child_process.execFile('/etc/alternatives/locate', [keyword, '-l', '10'], {}, function(err, stdout, stderr) {
-			if (err) {
-				throw err;
-			} else {
-				var result = stdout.split('\n');
-				$scope.searchResult = result;
-				console.log('$scope.searchResult:' + $scope.searchResult);
-			}
-		})
+                }
+            })
+        })
+    }
 
-		// child.stdout.once('data', function(data) {
-		// 	var result = data.split('\n');
-		// 	$scope.searchResult = result;
-		// 	console.log('result array:' + $scope.searchResult);
-		// });
-	}
+    $scope.selectSearchResult = function(index) {
+        $scope.selected = {
+            index: $scope.searchResult[index]
+        }
+    }
 
 
 })
