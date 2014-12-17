ea7b827eec01c7104481209f4bec85e2b6ece3ba
Rey <chen_m@worksap.co.jp>
2014-12-17 19:44:45 +0800

    move method

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('mainCtrl', function($scope) {
+    this.limit_ = 10;
+    $scope.selectedIndex = null;
     $scope.inputKeypress = function(event) {
         // enter
         if (event.keyCode === 13) {
@@ -8,9 +10,24 @@ mainApp.controller('mainCtrl', function($scope) {
         }
         // down arrow
         if (event.keyCode === 40) {
-            $scope.selectSearchResult(0);
+        	console.log("arrow down pressed in input");
+            selectSearchResult(0);
         }
     }
+
+    $scope.searchResultKeypress = function(event) {
+        if (event.keyCode === 13) {
+            openFile();
+        }
+        if (event.keyCode === 40) {
+            moveSelectCursorDown();
+        }
+        if (event.keyCode === 38) {
+            moveSelectCursorUp();
+        }
+    }
+
+
 
     $scope.searchFile = function(keyword) {
         console.warn(keyword);
@@ -29,8 +46,29 @@ mainApp.controller('mainCtrl', function($scope) {
         })
     }
 
-    $scope.selectSearchResult = function(index) {
-        $scope.selected = {
+    function openFile() {
+
+    }
+
+    function moveSelectCursorDown() {
+        if ($scope.selectedIndex === this.limit_ - 1) {
+            $scope.selectedIndex = 0;
+        } else {
+            $scope.selectedIndex = $scope.selectedIndex + 1;
+        }
+    }
+
+    function moveSelectCursorUp() {
+        if ($scope.selectedIndex === 0) {
+            $scope.selectedIndex = this.limit_ - 1;
+        } else {
+            $scope.selectedIndex = $scope.selectedIndex - 1;
+        }
+    }
+
+    function selectSearchResult(index) {
+        $scope.selectedIndex = index;
+        $scope.selectedItem = {
             index: $scope.searchResult[index]
         }
     }
